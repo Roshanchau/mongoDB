699d4c71adf9f795ac4d1985a6d244034c530d36
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-02-02 12:21:45 +0545

    patch request

--- app.js
+++ app.js
@@ -80,3 +80,20 @@ app.delete("/books/:id", (req, res) => {
     res.status(500).json({ error: "Not a vlid doc id" });
   }
 });
+
+app.patch("/books/:id", (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: updates })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "couldnot update the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a vlid doc id" });
+  }
+});
